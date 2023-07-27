32c02b7133e3a21d6d87b0077df60a64a444b246
K4R7IK <kartik.workspace@gmail.com>
2023-07-27 15:31:24 +0530

    Added Ai

--- src/pages/Customizer.jsx
+++ src/pages/Customizer.jsx
@@ -48,15 +48,18 @@ const Customizer = () => {
     if (!prompt) return alert("Please Enter a Prompt");
     try {
       setGeneratingImg(true);
-      const response = await fetch("http://localhost:8080/api/v1/dalle", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          prompt,
-        }),
-      });
+      const response = await fetch(
+        "https://tshirt-ai-sgb3.onrender.com/api/v1/dalle",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            prompt,
+          }),
+        }
+      );
       const data = await response.json();
       handleDecals(type, `data:image/png;base64,${data.photo}`);
     } catch (error) {
